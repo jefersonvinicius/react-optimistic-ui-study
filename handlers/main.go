830d28faac0fdf16f2af0cab249cefd4d95d3763
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2020-11-16 20:44:34 -0300

    Add bootstrap style in main page; Add progress bar

--- handlers/main.go
+++ handlers/main.go
@@ -13,8 +13,12 @@ func Main(w http.ResponseWriter, r *http.Request) {
 	var tasks []models.Task
 	database.Instance().Find(&tasks)
 
+	var numberOfTasksCompleted int64
+	database.Instance().Model(&models.Task{}).Where("completed = ?", true).Count(&numberOfTasksCompleted)
+
 	data := map[string]interface{}{
-		"tasks": tasks,
+		"tasks":    tasks,
+		"progress": float64(numberOfTasksCompleted) / float64(len(tasks)) * 100,
 	}
 	engine.RenderView(w, "main", data)
 }
