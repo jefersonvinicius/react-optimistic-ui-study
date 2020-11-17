75e7e5b52af7f7734af4b2f5eef9692153bd13d4
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2020-11-17 08:47:23 -0300

    Setup server static

--- http/router.go
+++ http/router.go
@@ -1,15 +1,25 @@
 package http
 
 import (
+	"net/http"
 	"todolist/handlers"
 
 	"github.com/gorilla/mux"
 )
 
+var router = mux.NewRouter()
+
 // GetRouter get router
 func GetRouter() *mux.Router {
-	router := mux.NewRouter()
-	router.HandleFunc("/", handlers.Main)
-
 	return router
+}
+
+// RegisterRoutes register routes
+func RegisterRoutes() {
+	router.HandleFunc("/", handlers.Main)
+}
+
+// SetupStaticFilesServer to serve static files
+func SetupStaticFilesServer() {
+	router.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
 }
--- http/server.go
+++ http/server.go
@@ -7,8 +7,13 @@ import (
 
 // InitServer init server
 func InitServer() {
+	SetupStaticFilesServer()
+	RegisterRoutes()
+
 	router := GetRouter()
+
 	http.Handle("/", router)
+
 	fmt.Println("Serving in http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
